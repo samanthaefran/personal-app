3102beb7f77622ec913dd6665e7ff19971230ccd
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-04-13 16:30:08 -0400

    basic set up

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return ( 
+  <Layout pageTitle="About">
+    <p>Part-time cyclist, full-time software dev trying to stay fit and make fun stuff</p>
+  </Layout>
+  )
+}
+
+export default AboutPage
