aed06fcc4bd9e7e36f3e1b103268d032ffe53281
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-04-14 17:05:44 -0400

    updates blog folder

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,31 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return(
     <Layout pageTitle="The Latest">
-      <p>My words will flood this page. Maybe it's nonsense, maybe it's profound.</p>
+       <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default BlogPage
