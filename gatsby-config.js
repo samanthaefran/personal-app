e86237fe4c84efddd3d3dc135c63aaf353b88330
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2022-04-14 08:18:13 -0400

    updates to styling, layout, new page route, and blog folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,12 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPage = () => {
+  return(
+    <Layout pageTitle="The Latest">
+      <p>My words will flood this page. Maybe it's nonsense, maybe it's profound.</p>
+    </Layout>
+  )
+}
+
+export default BlogPage
